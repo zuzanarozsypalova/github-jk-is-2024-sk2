8b5a1e1eaa18a46eeab018e80e8839dedc7f5fcb
Jiří Kvajsar <jiri.kvajsar@tul.cz>
2024-10-10 13:31:28 +0200

    1# základní program s možností opakování

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program {
+
+    static void Main(){
+
+        // chci, aby se program opakoval po stisku klávesy a 
+        string again = "a";
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++ Výpis řady +++++++");
+            Console.WriteLine("++++++++++++++++++++++++++");
+            Console.WriteLine("+++++++ Jiří Kvajsar +++++");
+            Console.WriteLine("++++++++++++++++++++++++++\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatele - TO-DO
+
+            // Logika pro výpis řady - TO-DO
+
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+
+
+        }            
+
+    }
+
+
+
+}
